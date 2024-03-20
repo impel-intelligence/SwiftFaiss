8a37c33b3e1183b2d5b4ddf9670728fd94628b26
Taylor Lineman <taylor.lineman@gmail.com>
2024-03-20 17:33:26 -0400

    Updated load function

--- Sources/SwiftFaiss/IndexIO.swift
+++ Sources/SwiftFaiss/IndexIO.swift
@@ -1,5 +1,9 @@
 import SwiftFaissC
 
+enum IOError: Error {
+    case creationFailed
+}
+
 public enum IOFlag {
     case mmap
     case readOnly
@@ -14,11 +18,17 @@ public enum IOFlag {
     }
 }
 
-public func loadFromFile(_ fileName: String, ioFlag: IOFlag = .readOnly) throws -> AnyIndex {
+public func loadFromFile<T: BaseIndex>(_ fileName: String, ioFlag: IOFlag = .readOnly) throws -> T {
     let indexPtr = UnsafeMutablePointer<OpaquePointer?>.allocate(capacity: 1)
     defer { indexPtr.deallocate() }
     try IndexError.check(
         faiss_read_index_fname(fileName, ioFlag.faissIOFlag, indexPtr)
     )
-    return AnyIndex(indexPointer: IndexPointer(indexPtr.pointee!))
+    
+    let index = T.from(IndexPointer(indexPtr.pointee!))
+    guard let index else {
+        throw IOError.creationFailed
+    }
+    
+    return index
 }
