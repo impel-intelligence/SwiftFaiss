e1ee23998f7c8a2e52da0ef39dda6d0c47355fbd
Taylor Lineman <git@actuallytaylor.com>
2026-06-25 16:11:47 -0400

    Add x86 binaries for Faiss
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- Package.swift
+++ Package.swift
@@ -13,10 +13,24 @@ let package = Package(
     products: [
         .library(name: "SwiftFaiss", targets: ["SwiftFaiss"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/impel-intelligence/FaissMobile", from: "1.0.0")
-    ],
     targets: [
+        .binaryTarget(
+            name: "FAISS",
+            url: "https://github.com/impel-intelligence/faiss-mobile/releases/download/v1.14.3-2/faiss.xcframework.zip",
+            checksum: "1ac5b9dce231e257031a26fea2c02cc54e84de8e86727e2b93800414d2b06f55"
+        ),
+        .binaryTarget(
+            name: "FAISS_C",
+            url: "https://github.com/impel-intelligence/faiss-mobile/releases/download/v1.14.3-2/faiss_c.xcframework.zip",
+            checksum: "79beb58ad88baf603a7de9caf20cbb1ba68ad043d9c4c3e1f3003d7ec60d882e"
+        ),
+        .binaryTarget(
+            name: "OpenMP",
+            url: "https://github.com/impel-intelligence/openmp-mobile/releases/download/v21.1.8/openmp.xcframework.zip",
+            checksum: "2f93a8273d648ab7b6d2c72c11a1684a8e63e0a1e6e6f2a326dae882d7acc20f"
+        ),
+
+//
         .target(
             name: "SwiftFaiss",
             dependencies: [
@@ -26,9 +40,9 @@ let package = Package(
         .target(
             name: "SwiftFaissC",
             dependencies: [
-                .product(name: "FAISS", package: "FaissMobile"),
-                .product(name: "FAISS_C", package: "FaissMobile"),
-                .product(name: "OpenMP", package: "FaissMobile"),
+                "FAISS",
+                "FAISS_C",
+                "OpenMP",
             ]
         ),
         .testTarget(
